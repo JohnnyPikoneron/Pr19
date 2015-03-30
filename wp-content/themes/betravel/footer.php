4b4cda60a7f348b128ff0d47441489e7b52a10e7
JohnnyPikoneron <dj_serje@inbox.ru>
2015-03-30 19:47:32 +0300

    new files

---
+++ wp-content/themes/betravel/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage betravel
+ * @since betravel 
+ */
+?>
+
+	
+
+
+
+<div id="footer">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">New Products </a></li>
+					<li><a href="#">Specials</a></li>
+					<li><a href="#">Products</a></li>
+					<li><a href="#">All Reviews </a></li>
+					<li><a href="#">Contact Us </a></li>
+					<li><a href="#">FAQ</a></li>
+				</ul>
+				<p>Copyright &copy; 2011 Dolce Vita.  Designed By : TemplatesPerfect</p>
+				<span>eye</span>
+			</div><!--end footer-->
+		</div><!--end wrap-->
+	<?php wp_footer(); ?>
+	</body>
+</html>
+
+
+
